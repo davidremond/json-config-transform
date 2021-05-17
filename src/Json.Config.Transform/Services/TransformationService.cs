c0401c39ef786ee5cb4a49409005f1361bc6d6db
David Remond <david.remond@scam.fr>
2021-05-17 11:03:31 +0200

    remove global throw

--- src/Json.Config.Transform/Services/TransformationService.cs
+++ src/Json.Config.Transform/Services/TransformationService.cs
@@ -24,88 +24,84 @@ namespace Json.Config.Transform.Services
 
         public async Task ApplyAsync(string sourceFilePath, string configFilePath, bool displayResult)
         {
-            try
+            // Check
+            if (string.IsNullOrEmpty(sourceFilePath))
+                throw new ArgumentNullException(nameof(sourceFilePath));
+            if (string.IsNullOrEmpty(configFilePath))
+                throw new ArgumentNullException(nameof(configFilePath));
+            _console.WriteLine("Parameters :");
+            _console.WriteLine($"  - Source File Path : {sourceFilePath}");
+            _console.WriteLine($"  - Config File Path : {configFilePath}");
+
+            // Get source file content
+            var configContent = await File.ReadAllTextAsync(configFilePath);
+
+            // Get operations
+            _console.WriteLine($"Get transformations");
+            var matches = regex.Matches(configContent);
+            JsonPatchDocument patchDocument = new JsonPatchDocument();
+            foreach (Match match in matches)
             {
-                // Check
-                if (string.IsNullOrEmpty(sourceFilePath))
-                    throw new ArgumentNullException(nameof(sourceFilePath));
-                if (string.IsNullOrEmpty(configFilePath))
-                    throw new ArgumentNullException(nameof(configFilePath));
-                _console.WriteLine("Parameters :");
-                _console.WriteLine($"  - Source File Path : {sourceFilePath}");
-                _console.WriteLine($"  - Config File Path : {configFilePath}");
-
-                // Get source file content
-                var configContent = await File.ReadAllTextAsync(configFilePath);
-
-                // Get operations
-                var matches = regex.Matches(configContent);
-                JsonPatchDocument patchDocument = new JsonPatchDocument();
-                foreach (Match match in matches)
+                var opType = match.Groups[1].Value;
+                var path = match.Groups[2].Value;
+                var value = match.Groups[3].Value.Trim(' ', '\r');
+                object objectValue = null;
+                try
                 {
-                    var opType = match.Groups[1].Value;
-                    var path = match.Groups[2].Value;
-                    var value = match.Groups[3].Value.Trim(' ', '\r');
-                    object objectValue = null;
-                    try
-                    {
-                        objectValue = JsonConvert.DeserializeObject(value);
-                    }
-                    catch (Exception)
-                    {
-                        objectValue = value;
-                    }
-                    patchDocument.Operations.Add(new Operation
-                    {
-                        op = opType,
-                        path = path,
-                        value = objectValue
-                    });
+                    objectValue = JsonConvert.DeserializeObject(value);
                 }
-
-                // Apply transformation
-                string jsonContent = await File.ReadAllTextAsync(sourceFilePath);
-                var jsonObject = JsonConvert.DeserializeObject(jsonContent);
-                var errors = new Dictionary<string, string>();
-                patchDocument.ApplyTo(jsonObject, err => { errors.Add($"{err.Operation.op} : {err.Operation.path}", err.ErrorMessage); });
-
-                // Log errors
-                if (errors.Any())
+                catch (Exception)
                 {
-                    _console.WriteLine();
-                    _console.WriteLine("Errors :");
-                    foreach (var error in errors)
-                    {
-                        _console.WriteLine($"  - {error.Key}");
-                        _console.WriteLine();
-                        _console.WriteLine($"    {error.Value}");
-                        _console.WriteLine();
-                    }
+                    objectValue = value;
                 }
-
-                // Save file
-                jsonContent = JsonConvert.SerializeObject(jsonObject);
-                await File.WriteAllTextAsync(sourceFilePath, jsonContent);
-
-                // Log result
-                if (displayResult)
+                patchDocument.Operations.Add(new Operation
                 {
-                    _console.WriteLine("Result :");
+                    op = opType,
+                    path = path,
+                    value = objectValue
+                });
+            }
+
+            // Apply transformation
+            _console.WriteLine($"Apply transformations");
+            string jsonContent = await File.ReadAllTextAsync(sourceFilePath);
+            var jsonObject = JsonConvert.DeserializeObject(jsonContent);
+            var errors = new Dictionary<string, string>();
+            patchDocument.ApplyTo(jsonObject, err => { errors.Add($"{err.Operation.op} : {err.Operation.path}", err.ErrorMessage); });
+
+            // Log errors
+            if (errors.Any())
+            {
+                _console.WriteLine();
+                _console.WriteLine("Errors :");
+                foreach (var error in errors)
+                {
+                    _console.WriteLine($"  - {error.Key}");
                     _console.WriteLine();
-                    using (var stringReader = new StringReader(jsonContent))
-                    using (var stringWriter = new StringWriter())
-                    {
-                        var jsonReader = new JsonTextReader(stringReader);
-                        var jsonWriter = new JsonTextWriter(stringWriter) { Formatting = Formatting.Indented };
-                        jsonWriter.WriteToken(jsonReader);
-                        var formatted = stringWriter.ToString();
-                        formatted.Split("\r\n").ToList().ForEach(_ => _console.WriteLine(_));
-                    }
+                    _console.WriteLine($"    {error.Value}");
+                    _console.WriteLine();
                 }
             }
-            catch (Exception ex)
+
+            // Save file
+            _console.WriteLine($"Save source file");
+            jsonContent = JsonConvert.SerializeObject(jsonObject);
+            await File.WriteAllTextAsync(sourceFilePath, jsonContent);
+
+            // Log result
+            if (displayResult)
             {
-                throw new TransformationException("Unable to apply transformation. See inner exceptions for more details", ex);
+                _console.WriteLine("Result :");
+                _console.WriteLine();
+                using (var stringReader = new StringReader(jsonContent))
+                using (var stringWriter = new StringWriter())
+                {
+                    var jsonReader = new JsonTextReader(stringReader);
+                    var jsonWriter = new JsonTextWriter(stringWriter) { Formatting = Formatting.Indented };
+                    jsonWriter.WriteToken(jsonReader);
+                    var formatted = stringWriter.ToString();
+                    formatted.Split("\r\n").ToList().ForEach(_ => _console.WriteLine(_));
+                }
             }
         }
     }
